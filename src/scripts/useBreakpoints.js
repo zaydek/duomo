3452e54165386f593d523fdd6d18dd1ec1438790
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-25 05:03:21 +0900

    Added a useBreakpoints function helper

---
+++ src/scripts/useBreakpoints.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+const defaultBreakpoints = {
+	xs: 40 * 16, //  640px
+	sm: 48 * 16, //  768px
+	md: 56 * 16, //  896px
+	lg: 64 * 16, // 1024px
+	xl: 80 * 16, // 1280px
+}
+
+export default function useBreakpoints(breakpoints = defaultBreakpoints) {
+	const [xs, setXS] = React.useState(true)
+	const [sm, setSM] = React.useState(true)
+	const [md, setMD] = React.useState(true)
+	const [lg, setLG] = React.useState(true)
+	const [xl, setXL] = React.useState(true)
+
+	React.useLayoutEffect(() => {
+		const handler = e => {
+			setXS(window.innerWidth >= breakpoints.xs)
+			setSM(window.innerWidth >= breakpoints.sm)
+			setMD(window.innerWidth >= breakpoints.md)
+			setLG(window.innerWidth >= breakpoints.lg)
+			setXL(window.innerWidth >= breakpoints.xl)
+		}
+		window.addEventListener("resize", handler)
+		return () => {
+			window.removeEventListener("resize", handler)
+		}
+	}, [breakpoints])
+
+	return { xs, sm, md, lg, xl }
+}
