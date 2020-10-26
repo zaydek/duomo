d9bf6462c043846e6e185b1fc9181d3926042f7b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-26 10:58:54 +0900

    Updated the runtime script

--- src/scripts/sorcery.js
+++ src/scripts/sorcery.js
@@ -1,12 +1,26 @@
+// Ex:
+//
+// <script
+// 	id="sorcery-script"
+// 	src="/scripts/sorcery.js"
+// 	data-node-env={process.env.NODE_ENV}
+// 	data-ssr
+// />
 window.sorcery = {
 	__version: "0.3.x",
 
-	init({ ssr } = { ssr: false }) {
-		const ssrWrapper = !ssr ? fn => fn() : fn => setTimeout(fn, 0)
+	init() {
+		const script = document.getElementById("sorcery-script")
+		if (!script) {
+			throw new Error('Sorcery: no such <script id="sorcery-script" ...>')
+		}
+
+		// prettier-ignore
+		const ssrWrapper = script.getAttribute("data-ssr") !== "" && script.getAttribute("data-ssr") !== "true"
+			? fn => fn()
+			: fn => setTimeout(fn, 0)
 
 		ssrWrapper(() => {
-			document.body.classList.add("macos-retina")
-
 			const media = "matchMedia" in window && window.matchMedia("(prefers-color-scheme: dark)")
 			if (media) {
 				if (media.matches) {
@@ -17,14 +31,15 @@ window.sorcery = {
 				})
 			}
 
-			document.addEventListener("keydown", e => {
-				if (!e.ctrlKey && e.key === "d") {
-					sorcery.toggleDarkMode()
-				}
-				if (e.ctrlKey && e.key === "d") {
-					sorcery.toggleDebugMode()
-				}
-			})
+			if (script.getAttribute("data-node-env") !== "production") {
+				document.addEventListener("keydown", e => {
+					if (!e.ctrlKey && e.key === "d") {
+						sorcery.toggleDarkMode()
+					} else if (e.ctrlKey && e.key === "d") {
+						sorcery.toggleDebugMode()
+					}
+				})
+			}
 		})
 	},
 	toggleDebugMode() {
@@ -43,8 +58,8 @@ window.sorcery = {
 			document.body.removeAttribute("data-theme")
 		}
 	},
-};
+}
 
 ;(() => {
-	window.sorcery.init({ ssr: true })
+	window.sorcery.init()
 })()
