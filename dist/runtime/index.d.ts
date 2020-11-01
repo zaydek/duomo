d2f3c8d59b2f4a5f37238c5332fb47b846718364
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-01 22:41:38 +0900

    Removing dist from committed files

--- dist/runtime/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface ISorcery {
-    init(): void;
-    toggleDebugMode(): boolean;
-    toggleDarkMode(): boolean;
-}
-declare const Sorcery: ISorcery;
-export default Sorcery;
-//# sourceMappingURL=index.d.ts.map
