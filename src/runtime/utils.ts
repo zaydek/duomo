081b94e3445097421885f70f7fad05d269456210
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-04 13:53:48 +0900

    Updated CSS util functions to round with the exception of em

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,20 +1,31 @@
 // Ex:
 //
 // rem(16) -> 1rem
-export function rem(n: number) {
-	return n / 16 + "rem"
+export function rem(n: number): number | string {
+	const rounded = Math.round(n)
+	if (!rounded) {
+		return 0
+	}
+	return rounded / 16 + "rem"
 }
 
 // Ex:
 //
 // rem(16) -> 1em
-export function em(n: number) {
+export function em(n: number): number | string {
+	if (!n) {
+		return 0
+	}
 	return n / 16 + "em"
 }
 
 // Ex:
 //
 // rem(16) -> 16px
-export function px(n: number) {
-	return n + "px"
+export function px(n: number): number | string {
+	const rounded = Math.round(n)
+	if (!rounded) {
+		return 0
+	}
+	return rounded + "px"
 }
