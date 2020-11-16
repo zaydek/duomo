e4789e6efc14c13e1b0efb357e984bb749a2a712
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-16 12:34:31 +0900

    Added unit test with transition

--- src/duomo/theme.test.ts
+++ src/duomo/theme.test.ts
@@ -45,3 +45,52 @@ body[data-theme=dark] {
 }
 `.trim())
 })
+
+test("theme: with transition", () => {
+	const result = sass(`
+@use "src/duomo/configuration";
+@use "src/duomo/theme" as *;
+
+body {
+	@include theme(
+		$theme-map: (
+			--bg-body: (
+				property: background-color,
+				light: configuration.$white,
+				dark: configuration.$black,
+			),
+		),
+		$duration: 300ms,
+		$timing-function: linear,
+	);
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+body {
+	--bg-body: #fff;
+	--theme-transition: background-color 300ms linear, border-color 300ms linear, box-shadow 300ms linear, color 300ms linear, fill 300ms linear, opacity 300ms linear, stroke 300ms linear, transform 300ms linear;
+}
+
+body[data-theme=dark] {
+	--bg-body: #000;
+}
+
+.bg-body {
+	background-color: var(--bg-body);
+	transition: var(--theme-transition);
+}
+
+.on\\:bg-body:hover,
+.on\\:bg-body:focus,
+.group:hover .group-on\\:bg-body,
+.group:focus .group-on\\:bg-body {
+	background-color: var(--bg-body);
+}
+
+.down\\:bg-body:active,
+.group:active .group-down\\:bg-body {
+	background-color: var(--bg-body);
+}
+`.trim())
+})
