2536e6f4719dcf1a74604414b5128ed2acc0d139
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-27 01:23:42 +0900

    safe-negative-sign -> safe-get-sign

--- src/duomo/helpers/safe.test.ts
+++ src/duomo/helpers/safe.test.ts
@@ -8,7 +8,7 @@ test("integration", () => {
 @use "src/duomo/helpers/safe" as *;
 
 @mixin fn($v) {
-	.#{safe-negative-sign($v)}m-#{safe-abs($v)} {
+	.#{safe-get-sign($v)}m-#{safe-abs($v)} {
 		/**/
 	}
 }
