cd54ad0d2abe381740459c8d67cb5233f4081861
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-11-30 02:26:52 +0900

    Added an integration test for variant

--- src/duomo/helpers/generate-from-range.test.ts
+++ src/duomo/helpers/generate-from-range.test.ts
@@ -3,41 +3,13 @@
  */
 declare function sass(data: string): string
 
-test("variants sans responsive", () => {
+test("integration", () => {
 	const result = sass(`
 @use "src/duomo/helpers/generate-from-range" as *;
 
 @at-root {
 	@include generate-from-range((
 		variant: none,
-		shorthand: square,
-		property: (width, height),
-		range: (0, 2, 4, 8, 16),
-		resolver: rem,
-	));
-	@include generate-from-range((
-		variant: hover,
-		shorthand: square,
-		property: (width, height),
-		range: (0, 2, 4, 8, 16),
-		resolver: rem,
-	));
-	@include generate-from-range((
-		variant: focus,
-		shorthand: square,
-		property: (width, height),
-		range: (0, 2, 4, 8, 16),
-		resolver: rem,
-	));
-	@include generate-from-range((
-		variant: group-hover,
-		shorthand: square,
-		property: (width, height),
-		range: (0, 2, 4, 8, 16),
-		resolver: rem,
-	));
-	@include generate-from-range((
-		variant: group-focus,
 		shorthand: square,
 		property: (width, height),
 		range: (0, 2, 4, 8, 16),
@@ -68,106 +40,6 @@ test("variants sans responsive", () => {
 }
 
 .square-16 {
-	width: ${16 / 16}rem;
-	height: ${16 / 16}rem;
-}
-
-.hover\\:square-0:hover {
-	width: 0;
-	height: 0;
-}
-
-.hover\\:square-2:hover {
-	width: ${2 / 16}rem;
-	height: ${2 / 16}rem;
-}
-
-.hover\\:square-4:hover {
-	width: ${4 / 16}rem;
-	height: ${4 / 16}rem;
-}
-
-.hover\\:square-8:hover {
-	width: ${8 / 16}rem;
-	height: ${8 / 16}rem;
-}
-
-.hover\\:square-16:hover {
-	width: ${16 / 16}rem;
-	height: ${16 / 16}rem;
-}
-
-.focus\\:square-0:focus {
-	width: 0;
-	height: 0;
-}
-
-.focus\\:square-2:focus {
-	width: ${2 / 16}rem;
-	height: ${2 / 16}rem;
-}
-
-.focus\\:square-4:focus {
-	width: ${4 / 16}rem;
-	height: ${4 / 16}rem;
-}
-
-.focus\\:square-8:focus {
-	width: ${8 / 16}rem;
-	height: ${8 / 16}rem;
-}
-
-.focus\\:square-16:focus {
-	width: ${16 / 16}rem;
-	height: ${16 / 16}rem;
-}
-
-.group-hover:hover .group-hover\\:square-0 {
-	width: 0;
-	height: 0;
-}
-
-.group-hover:hover .group-hover\\:square-2 {
-	width: ${2 / 16}rem;
-	height: ${2 / 16}rem;
-}
-
-.group-hover:hover .group-hover\\:square-4 {
-	width: ${4 / 16}rem;
-	height: ${4 / 16}rem;
-}
-
-.group-hover:hover .group-hover\\:square-8 {
-	width: ${8 / 16}rem;
-	height: ${8 / 16}rem;
-}
-
-.group-hover:hover .group-hover\\:square-16 {
-	width: ${16 / 16}rem;
-	height: ${16 / 16}rem;
-}
-
-.group-focus:focus .group-focus\\:square-0 {
-	width: 0;
-	height: 0;
-}
-
-.group-focus:focus .group-focus\\:square-2 {
-	width: ${2 / 16}rem;
-	height: ${2 / 16}rem;
-}
-
-.group-focus:focus .group-focus\\:square-4 {
-	width: ${4 / 16}rem;
-	height: ${4 / 16}rem;
-}
-
-.group-focus:focus .group-focus\\:square-8 {
-	width: ${8 / 16}rem;
-	height: ${8 / 16}rem;
-}
-
-.group-focus:focus .group-focus\\:square-16 {
 	width: ${16 / 16}rem;
 	height: ${16 / 16}rem;
 }
---
+++ src/duomo/helpers/variant.test.ts
@@ -0,0 +1,65 @@
+/**
+ * @jest-environment node
+ */
+declare function sass(data: string): string
+
+test("integration", () => {
+	const result = sass(`
+@use "src/duomo/helpers/variant" as *;
+
+@mixin background-color($variants...) {
+	@include variants($variants) using ($v) {
+		#{variant(indigo-500, $v)} {
+			background-color: var(--indigo-500);
+		}
+	}
+}
+
+@at-root {
+	@include background-color(responsive, hover, focus, group-hover, group-focus);
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.indigo-500 {
+	background-color: var(--indigo-500);
+}
+
+.hover\\:indigo-500:hover {
+	background-color: var(--indigo-500);
+}
+
+.focus\\:indigo-500:focus {
+	background-color: var(--indigo-500);
+}
+
+.group-hover:hover .group-hover\\:indigo-500 {
+	background-color: var(--indigo-500);
+}
+
+.group-focus:focus .group-focus\\:indigo-500 {
+	background-color: var(--indigo-500);
+}
+
+@media (min-width: 640px) {
+	.sm\\:indigo-500 {
+		background-color: var(--indigo-500);
+	}
+}
+@media (min-width: 768px) {
+	.md\\:indigo-500 {
+		background-color: var(--indigo-500);
+	}
+}
+@media (min-width: 1024px) {
+	.lg\\:indigo-500 {
+		background-color: var(--indigo-500);
+	}
+}
+@media (min-width: 1280px) {
+	.xl\\:indigo-500 {
+		background-color: var(--indigo-500);
+	}
+}
+`.trim())
+})
