ce0bb3b55725b212af6565ed6f5b48e93cadd58c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-01 05:11:44 +0900

    cap now supports negatives as well

--- src/duomo/helpers/cap.test.ts
+++ src/duomo/helpers/cap.test.ts
@@ -3,7 +3,39 @@
  */
 declare function sass(data: string): string
 
-test("integration", () => {
+test("cap: negative-case", () => {
+	const result = sass(`
+@use "sass:math";
+
+@use "src/duomo/helpers/cap" as *;
+
+@each $rv in cap((-1, -2, -4, -8, -16, -32), -8) {
+	.-w-#{math.abs($rv)} {
+		width: $rv + px;
+	}
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.-w-1 {
+	width: -1px;
+}
+
+.-w-2 {
+	width: -2px;
+}
+
+.-w-4 {
+	width: -4px;
+}
+
+.-w-8 {
+	width: -8px;
+}
+`.trim())
+})
+
+test("cap: positive-case", () => {
 	const result = sass(`
 @use "src/duomo/helpers/cap" as *;
 
