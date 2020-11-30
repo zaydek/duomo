568e0bb44bfa3ac0d62beeb17a8025d48da72424
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-01 04:31:43 +0900

    Fixed variants test suite

--- src/duomo/helpers/variants.test.ts
+++ src/duomo/helpers/variants.test.ts
@@ -5,59 +5,60 @@ declare function sass(data: string): string
 
 test("integration", () => {
 	const result = sass(`
-@use "src/duomo/helpers/variant" as *;
+@use "src/duomo/helpers/variants" as *;
 
 @mixin background-color($variants...) {
 	@include variants($variants...) using ($v) {
-		#{variant(indigo-500, $v)} {
+		#{variant(bg-indigo-500, $v)} {
 			background-color: var(--indigo-500);
 		}
 	}
 }
 
 @at-root {
-	@include background-color(responsive, hover, focus, group-hover, group-focus);
+	@include background-color(none, responsive, hover, focus, group-hover, group-focus);
 }
 `)
+	console.log(result)
 	// prettier-ignore
 	expect(result).toBe(`
-.indigo-500 {
+.bg-indigo-500 {
 	background-color: var(--indigo-500);
 }
 
-.hover\\:indigo-500:hover {
+.hover\\:bg-indigo-500:hover {
 	background-color: var(--indigo-500);
 }
 
-.focus\\:indigo-500:focus {
+.focus\\:bg-indigo-500:focus {
 	background-color: var(--indigo-500);
 }
 
-.group-hover:hover .group-hover\\:indigo-500 {
+.group:hover .group-hover\\:bg-indigo-500 {
 	background-color: var(--indigo-500);
 }
 
-.group-focus:focus .group-focus\\:indigo-500 {
+.group:focus .group-focus\\:bg-indigo-500 {
 	background-color: var(--indigo-500);
 }
 
 @media (min-width: 640px) {
-	.sm\\:indigo-500 {
+	.sm\\:bg-indigo-500 {
 		background-color: var(--indigo-500);
 	}
 }
 @media (min-width: 768px) {
-	.md\\:indigo-500 {
+	.md\\:bg-indigo-500 {
 		background-color: var(--indigo-500);
 	}
 }
 @media (min-width: 1024px) {
-	.lg\\:indigo-500 {
+	.lg\\:bg-indigo-500 {
 		background-color: var(--indigo-500);
 	}
 }
 @media (min-width: 1280px) {
-	.xl\\:indigo-500 {
+	.xl\\:bg-indigo-500 {
 		background-color: var(--indigo-500);
 	}
 }
