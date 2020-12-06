c908f536fe5ffe28a1b0b15b66ef2a9384bb0a9e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-07 01:56:31 +0900

    Added bar limiter

---
+++ src/duomo/helpers/limiters.test.ts
@@ -0,0 +1,106 @@
+/**
+ * @jest-environment node
+ */
+declare function sass(data: string): string
+
+test("bar: negative case", () => {
+	const result = sass(`
+@use "sass:math";
+
+@use "src/duomo/helpers/limiters" as *;
+
+@each $rv in bar((0, -1, -2, -4), -2) {
+	.-w-#{math.abs($rv)} {
+		width: $rv + px;
+	}
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.-w-2 {
+	width: -2px;
+}
+
+.-w-4 {
+	width: -4px;
+}
+`.trim())
+})
+
+test("bar: positive case", () => {
+	const result = sass(`
+@use "sass:math";
+
+@use "src/duomo/helpers/limiters" as *;
+
+@each $rv in bar((0, 1, 2, 4), 2) {
+	.w-#{math.abs($rv)} {
+		width: $rv + px;
+	}
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.w-2 {
+	width: 2px;
+}
+
+.w-4 {
+	width: 4px;
+}
+`.trim())
+})
+
+test("cap: negative case", () => {
+	const result = sass(`
+@use "sass:math";
+
+@use "src/duomo/helpers/limiters" as *;
+
+@each $rv in cap((0, -1, -2, -4), -2) {
+	.-w-#{math.abs($rv)} {
+		width: $rv + px;
+	}
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.-w-0 {
+	width: 0px;
+}
+
+.-w-1 {
+	width: -1px;
+}
+
+.-w-2 {
+	width: -2px;
+}
+`.trim())
+})
+
+test("cap: positive case", () => {
+	const result = sass(`
+@use "src/duomo/helpers/limiters" as *;
+
+@each $rv in cap((0, 1, 2, 4), 2) {
+	.w-#{$rv} {
+		width: $rv + px;
+	}
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+.w-0 {
+	width: 0px;
+}
+
+.w-1 {
+	width: 1px;
+}
+
+.w-2 {
+	width: 2px;
+}
+`.trim())
+})
