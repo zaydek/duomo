fcc41adc6524d716cfb294960a8b9993c1301445
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-12 04:17:01 +0900

    Revised builder

---
+++ src/duomo/helpers/__tests/generate-vars.test.ts
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment node
+ */
+declare function sass(data: string): string
+
+test("integration", () => {
+	const result = sass(`
+@use "src/duomo/helpers/generate-vars" as *;
+
+@at-root {
+	@include generate-vars(
+		(
+			a: x,
+			b: y,
+			c: z,
+		),
+	);
+}
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+:root {
+	--a: x;
+	--b: y;
+	--c: z;
+}
+`.trim())
+})
