96f2a8615b4d243c240f4f0d2cb6a6b504af943d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-12-28 20:12:53 +0900

    Added unit test for themes

---
+++ src/sass/mixins/__tests/themes.test.ts
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment node
+ */
+declare function sass(data: string): string
+
+test("integration", () => {
+	const result = sass(`
+@use "src/sass/helpers" as *;
+@use "src/sass/mixins/themes" as *;
+
+$light: (
+	ui-app-color: color(black),
+	ui-app-bg: color(white),
+);
+
+$dark: (
+	ui-app-color: color(white),
+	ui-app-bg: color(black),
+);
+
+@include themes($light, $dark);
+`)
+	// prettier-ignore
+	expect(result).toBe(`
+:root {
+	--ui-app-color: hsl(0deg, 0%, 0%);
+	--ui-app-bg: hsl(0deg, 0%, 100%);
+}
+
+[data-theme=dark] {
+	--ui-app-color: hsl(0deg, 0%, 100%);
+	--ui-app-bg: hsl(0deg, 0%, 0%);
+}
+
+.ui-app {
+	color: var(--ui-app-color);
+	background-color: var(--ui-app-bg);
+	transition: var(--theme-transition);
+}
+`.trim())
+})
