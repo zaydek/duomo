95cf862fa0c638a525d7c4753f57e8fbce92f2d5
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-01-03 21:58:17 +0900

    :not([data-theme-effect]) -> :root:not([data-theme-effect])

---
+++ tests/mixins/themes.DEPRECATE.test.ts
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment node
+ */
+declare function sass(data: string): string
+
+test("integration", () => {
+	const css = sass(`
+@use "index" as * with ($headless: true);
+
+$light: (
+	app-color: color(black),
+	app-bg: color(white),
+);
+
+$dark: (
+	app-color: color(white),
+	app-bg: color(black),
+);
+
+@at-root {
+	@include themes((
+		light: $light,
+		dark: $dark,
+		duration: 700ms,
+		timing: timing(ease-out),
+	));
+}
+`)
+	// prettier-ignore
+	expect(css).toBe(`
+:root {
+	--app-color: hsl(0deg, 0%, 0%);
+	--app-bg: hsl(0deg, 0%, 100%);
+	--default-theme-transition-duration: 700ms;
+	--default-theme-transition-timing: cubic-bezier(0, 0, 0.2, 1);
+}
+
+[data-theme=dark] {
+	--app-color: hsl(0deg, 0%, 100%);
+	--app-bg: hsl(0deg, 0%, 0%);
+}
+
+.x-app {
+	color: var(--app-color);
+	background-color: var(--app-bg);
+	transition: var(--theme-transition);
+}
+`.trim())
+})
